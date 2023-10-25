4399239ecfe24451c46dd496c464ddc84571ce7e
sonie <garachu.muthoni@gmail.com>
2023-10-24 19:06:27 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """
+        Initialize a square with a given size.
+        :param size: The size of the square
+        """
+
+        self.__size = size
