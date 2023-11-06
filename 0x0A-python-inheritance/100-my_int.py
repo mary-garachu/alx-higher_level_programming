93bf112eb2d8554e2f2e8b7298e5256df1ea9daa
sonie <garachu.muthoni@gmail.com>
2023-11-06 13:43:43 -0800

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python3
-'''Module for MyInt class.'''
+"""
+=============================
+Module with a class Myint
+=============================
+"""
 
 
 class MyInt(int):
+    """class that inherits from int"""
+
+    def __init__(self, my_int):
+        """for initialize a value my_int"""
+
+        self.my_int = my_int
+
+    @property
+    def my_int(self):
+        return self.__my_int
+
+    @my_int.setter
+    def my_int(self, my_int):
+        if type(my_int) is not int:
+            raise TypeError("my_int must be an integer")
+        else:
+            self.__my_int = my_int
+
     def __eq__(self, other):
-        '''Override equals, inverting it.'''
-        return int(self) != int(other)
+        """equal method"""
+
+        if self.my_int == other:
+            return False
+        else:
+            return True
 
     def __ne__(self, other):
-        '''Override not-equals, inverting it.'''
-        return int(self) == int(other)
+            if self.my_int != other:
+                return False
+            else:
+                return True
