a52e63c5a3cb00037c2a8880bdfe4a03d280ea1c
sonie <garachu.muthoni@gmail.com>
2023-11-07 18:02:44 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,5 +6,4 @@ Module with append function
 
 def append_write(filename="", text=""):
     with open(filename, 'a', encoding='utf-8') as file:
-        num_chars_added = file.write(text)
-    return num_chars_added
+        return file.write(text)
