eaf3176c018efb056b83b7ebe7a922abbf1ca454
sonie <garachu.muthoni@gmail.com>
2024-01-10 02:26:35 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
 for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
   console.log(line);
 }
