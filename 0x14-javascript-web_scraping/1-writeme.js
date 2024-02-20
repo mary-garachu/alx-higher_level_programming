596e7e2cd2c7a8850675f77564a96f40d1c8f407
Sonie <garachu.muthoni@gmail.com>
2024-02-20 05:21:53 -0800

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,19 +2,8 @@
 
 const fs = require('fs');
 
-// Check if both file path and string to write are provided as arguments
-if (process.argv.length < 4) {
-  console.error('Please provide both a file path and a string to write.');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
   }
-  console.log(`String "${stringToWrite}" has been written to ${filePath}`);
 });
